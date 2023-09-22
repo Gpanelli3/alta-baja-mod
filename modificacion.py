9b9d50233e04948ec351f38be7a9e71020ca5470
gpanelli3 <gpanelli3@gmail.com>
2023-09-22 19:59:13 -0300

    'sque-id-Alta'

---
+++ modificacion.py
@@ -0,0 +1,9 @@
+import cgi 
+import mysql.connector
+
+
+conexion=mysql.connector.connect(
+                        host='localhost':8000,
+                        host="probando-bases",
+                         user="",
+                         ) 
--- vistahtml.py
+++ vistahtml.py
@@ -66,7 +66,6 @@ print("""<html>
             <br>
             <form method="post" action="alta.py">
         	    <p>Ingreso de alumnos</p>
-                id:<input type="number" name="id" /> <br/>
                 nombre: <input type="text" name="nombre"  /> <br/>
                 apellido: <input type="text" name="apellido" /> <br/>
                 <button>INGRESAR ALUMNO</button>
